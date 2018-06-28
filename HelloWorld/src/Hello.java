2ef1489f09239a04180712a4a78b4e36e9c41198
Patrick McMahon <pmcmahon14@gmail.com>
2018-06-27 23:16:33 -0700

    added HelloWorld file

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Patrick!");
+
+        int myFirstNumber = 10 + 5 * 3;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myNewTotal = 1000 - myTotal;
+
+        System.out.println(myNewTotal);
+    }
+}
---
+++ HelloWorld/src/com/patricksmcmahon/Main.java
@@ -0,0 +1,8 @@
+package com.patricksmcmahon;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
