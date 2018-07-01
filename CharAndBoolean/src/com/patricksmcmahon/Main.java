b631b9275f36883e40917b3375cfa96619c26d8f
Patrick McMahon <pmcmahon14@gmail.com>
2018-06-30 23:25:56 -0700

    finished CharAndBoolean types

---
+++ CharAndBoolean/src/com/patricksmcmahon/Main.java
@@ -0,0 +1,14 @@
+package com.patricksmcmahon;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        System.out.println("Unicode output is: "+ myChar);
+
+        boolean myBoolean = false;
+
+        char newChar = '\u00AE';
+        System.out.println("New Unicode output is: " + newChar);
+    }
+}
